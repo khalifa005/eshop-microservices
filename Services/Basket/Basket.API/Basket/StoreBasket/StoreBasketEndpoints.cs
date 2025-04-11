dcc68401b437319604ac3656f43db29a8d13a58a
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2025-04-11 14:53:55 +0300

    added store basket

--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,5 +1,21 @@
+using Carter;
+using MediatR;
+
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoints
+public class StoreBasketEndpoints : ICarterModule
 {
+  public void AddRoutes(IEndpointRouteBuilder app)
+  {
+    app.MapPost("/basket", async (StoreBasketCommand request, ISender sender) =>
+    {
+      var result = await sender.Send(request);
+      return Results.Created($"/basket/{result.Username}", result);
+    })
+      .WithName("CreateProduct")
+      .Produces<StoreBasketResult>(StatusCodes.Status201Created)
+      .ProducesProblem(StatusCodes.Status400BadRequest)
+      .WithSummary("Create Product")
+    ;
+  }
 }
--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,35 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using FluentValidation;
 
-public class StoreBasketHandler
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string Username);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+  public StoreBasketCommandValidator()
+  {
+    RuleFor(x => x.Cart)
+        .NotNull()
+        .WithMessage("Cart cannot be null");
+
+    RuleFor(x => x.Cart.UserName)
+        .NotEmpty()
+        .WithMessage("UserName cannot be empty");
+  }
 }
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+  public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+  {
+    ShoppingCart cart = command.Cart;
+
+
+    //TODO: Logic to store the basket
+    //TODO:update cache 
+    return new StoreBasketResult("khalifa");
+  }
+}
+
